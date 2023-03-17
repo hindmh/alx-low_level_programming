ae791d1a6cc7fd170f395567c0ad4f3e5f438312
hindmh <elmohrir.hind@gmail.com>
2023-03-16 17:40:58 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-  * main -entry point
-  * Return: 1 (success)
-  */
- 
-int main()
+ * main -entry point
+ * Return: 1 (success)
+ */
+int main(void)
 {
-    fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
-    return (1);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	return (1);
 }
