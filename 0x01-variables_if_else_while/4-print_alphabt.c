0e849a9dcbe16af9c8dca9a312807357d742ce8e
hindmh <elmohrir.hind@gmail.com>
2023-03-17 17:02:08 -0700

    print alphabet 3

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  * main -entry point
+  * Return: 0 (success)
+  */
+int main(void)
+{
+	int x = 97;
+
+	while (x <= 122)
+	{
+		if (x == 101 || x == 113)
+		{
+			x++;
+			continue;
+		}
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
