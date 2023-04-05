6351d466bbe8306385257f5f7e38be4c1a967412
hindmh <elmohrir.hind@gmail.com>
2023-04-05 16:52:44 -0700

    sqrt recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,7 +11,7 @@ int _sqrt_recursion(int n)
 		return (-1);
 	return (actual_sqrt_recursion(n, 0));
 }
-
+int actual_sqrt_recursion(int n, int m);
 /**
  * actual_sqrt_recursion - recurses to find the natural
  * square root of a number
