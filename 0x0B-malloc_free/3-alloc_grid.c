e4fa0d138198847ac22a3ea324c9077481222222
hindmh <elmohrir.hind@gmail.com>
2023-04-11 17:44:55 -0700

    alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,6 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	mee = malloc(sizeof(int *) * height);
-	
 	if (mee == NULL)
 		return (NULL);
 	for (x = 0; x < height; x++)
